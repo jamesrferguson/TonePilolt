19df43c3f6cd225daa46207ebc41c1a4b778a938
James Ferguson <jamesrferguson101@gmail.com>
2023-10-28 08:13:01 +0100

    Adding base templates for app.

---
+++ app/tutorial/page.tsx
@@ -0,0 +1,9 @@
+export default function() {
+    return (
+        <div className="app-container">
+            <h1>Tutorial and Tips</h1>
+            <p>Learn how to use Song Smith and improve your music theory knowledge.</p>
+            {/* Tutorial and Tips Content Goes Here */}
+        </div>
+    )
+}
